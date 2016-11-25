1ce6b131e44f03e0f2e6c7a5431f644f5f1d9be5
Zina Al-Sabbagh <14zas@queensu.ca>
2016-11-24 23:24:37 -0500

    first commit

---
+++ capitalizer.c
@@ -0,0 +1,42 @@
+// Rita Alsattah 10154610
+// Zina Al-Sabbagh 10161380
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(){
+	
+	char poem[];
+	int c;
+	
+	scanf("%s", poem);
+
+	
+	FILE *file;
+	file = fopen(poem, "w+");
+
+	char prevc='\n';
+
+	if (file) {
+		while ((c = getc(file)) != EOF) {
+			if (c>= 'a' && c <= 'z' && (prevc=='\n')) {
+                
+				int upperc = toupper(c);
+				
+				fprintf(file, "%c",upperc);
+				
+				putchar(upperc);
+			}
+			else{	
+				
+				fprintf(file, "%c",c);
+                
+				putchar(c);
+			}
+
+		prevc=c;
+		}
+	}
+}
